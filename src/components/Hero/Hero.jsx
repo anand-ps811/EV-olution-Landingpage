3fa1932e2bd339eea08e186bb9c458a7017e9f1c
anand-ps811 <anandps.stackuptvm@gmail.com>
2024-12-16 11:39:34 +0530

    change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import './hero.css';
+import  './Hero.css'
 import arrow_btn from '../../Assets/arrow_btn.png';
 import play_icon from '../../Assets/play_icon.png';
 import pause_icon from '../../Assets/pause_icon.png';
