e31e5a56b62a719ceb01132a7098ad11e23f2a41
anand-ps811 <164320352+anand-ps811@users.noreply.github.com>
2025-07-14 11:54:33 +0530

    Create thankspage.jsx

---
+++ src/components/Navbar/thankspage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ThanksPage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '4rem' }}>
+      <h1>🎉 Thanks for clicking!</h1>
+      <p>This is the /thanks route.</p>
+    </div>
+  );
+}
+
+export default ThanksPage;
