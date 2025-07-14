93a9939c11b0d5d1924541f9d09e898fe917175d
anand-ps811 <164320352+anand-ps811@users.noreply.github.com>
2025-07-14 11:53:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Background from "./components/Background/Background.jsx";
-import  Navbar  from "./components/Navbar/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
-import './index.css'
+import './index.css';
+import ThanksPage from './components/Navbar/thankspage.jsx';
+import Homepage from "./homepage.jsx";
+
 export const App = () => {
-  let heroData = [
-    {text1:'Dive into',text2:'what you love'},
-    {text1:'Indulge',text2:'your passions'},
-    {text1:'Give in to ',text2:'your passions'}, 
-
-  ]
-  const [heroCount,setHeroCount]=useState(0);
-  const [playStatus,setPlayStatus]=useState(false)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHeroCount((count) => (count === 2 ? 0 : count + 1));
-    }, 3000);
-  
-    return () => clearInterval(interval); // Cleanup on unmount.
-  }, []);
-  
-
   return (
-    <div>
-      <Background playStatus={playStatus} heroCount={heroCount}/>
-      <Navbar/>
-      <Hero
-      setPlayStatus={setPlayStatus}
-      heroData={heroData[heroCount]}
-      heroCount={heroCount}
-      setHeroCount={setHeroCount}
-      playStatus={playStatus}
-      />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/thanks" element={<ThanksPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
